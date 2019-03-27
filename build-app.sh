30e14273f20cca171a52f1f6ad7459c416963134
Wojciech Kocjan <wojciech@bitnami.com>
2019-03-27 16:53:00 +0100

    Add logic for building and initializing the application

--- build-app.sh
+++ build-app.sh
@@ -3,8 +3,17 @@
 set -eu -o pipefail
 
 if [ "${STACKSMITH_NEW_APP}" != "" ] ; then
-  echo "INIT"
+  if [ "${STACKSMITH_NEW_APP_NAME}" = "" ] ; then
+    echo >&2 "Application name must be specified"
+    exit 1
+  fi
+
+  ./init-new-app.sh aws-demo-bitnami "${STACKSMITH_NEW_APP_NAME}"
 else
-  echo "NO INIT"
+  echo "Building existing application"
 fi
+
+stacksmith build
+
+git reset --hard
 
